a6e2357dbab4664cae42a7e2c1783058bd556bd2
Mohit Soni <developer_mohitsoni@outlook.com>
2025-01-17 00:35:01 +0530

    redirect authenticated users to the dashboard from the Login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -7,10 +7,14 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { signIn } from "../utils/auth";
+import { auth, signIn } from "../utils/auth";
 import { SubmitButton } from "@/components/SubmitButton";
+import { redirect } from "next/navigation";
 
-export default function Login() {
+export default async function Login() {
+  const session = await auth();
+
+  if (session?.user) redirect("/dashboard");
   return (
     <>
       <div className="flex h-screen w-full items-center justify-center px-4">
