463da395c19c6ea90c741e441c8af9b9cba6be11
Mohit Soni <developer_mohitsoni@outlook.com>
2025-01-17 00:24:42 +0530

    add sign-out functionality to DashboardRoute with a sign-out button

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,4 @@
+import { signOut } from "../utils/auth";
 import { requireUser } from "../utils/hooks";
 
 export default async function DashboardRoute() {
@@ -5,6 +6,14 @@ export default async function DashboardRoute() {
   return (
     <div>
       <h1>Hello from the Dashboard Route</h1>
+      <form
+        action={async () => {
+          "use server";
+          await signOut();
+        }}
+      >
+        <button type="submit">Sign Out</button>
+      </form>
     </div>
   );
 }
